5e864a77c7f9cb40b05a2f6440191880d49b5074
Maarten De Paepe <maarten@adimian.com>
2021-12-17 13:42:30 +0100

    day seventeen

---
+++ day17/solver.py
@@ -0,0 +1,74 @@
+import re
+from runner_utils import expected_test_result
+
+FIELD_REGEX = "target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)"
+
+
+class Rect:
+    def __init__(self, sx, ex, sy, ey):
+        sx, ex, sy, ey = int(sx), int(ex), int(sy), int(ey)
+
+        self.sx = min(sx, ex)
+        self.ex = max(sx, ex)
+        self.sy = max(sy, ey)
+        self.ey = min(sy, ey)
+
+    def contains(self, point):
+        within_x = self.sx <= point[0] <= self.ex
+        within_y = self.sy >= point[1] >= self.ey
+        return within_x and within_y
+
+    def __repr__(self):
+        return f"({self.sx},{self.sy}),({self.ex},{self.ey})"
+
+
+def find_highest(field):
+    initial_interval = 0
+    highest = 0
+    found_interval = 0
+    while initial_interval > field.ey:
+        y = initial_interval
+        interval = initial_interval
+        while y > field.ey:
+            if field.contains((field.sx, y)):
+                highest = min(highest, y)
+                if highest == y:
+                    found_interval = initial_interval
+            interval -= 1
+            y += interval
+        initial_interval -= 1
+    return sum(range(abs(found_interval) + 1))
+
+
+def find_valid_velocities(field):
+    initial_x_velocity = 0
+    while initial_x_velocity <= field.ex:
+        initial_y_velocity = abs(field.ey)
+        while initial_y_velocity >= field.ey:
+            x = 0
+            y = 0
+            x_velocity = initial_x_velocity
+            y_velocity = initial_y_velocity
+
+            while y >= field.ey:
+                x += x_velocity
+                y += y_velocity
+                if field.contains((x, y)):
+                    yield initial_x_velocity, initial_y_velocity
+                x_velocity = max(x_velocity - 1, 0)
+                y_velocity -= 1
+            initial_y_velocity -= 1
+        initial_x_velocity += 1
+
+
+@expected_test_result(45)
+def solve1(input):
+    field = Rect(*re.match(FIELD_REGEX, input).groups())
+    return find_highest(field)
+
+
+@expected_test_result(112)
+def solve2(input):
+    field = Rect(*re.match(FIELD_REGEX, input).groups())
+    velocities = set(find_valid_velocities(field))
+    return len(velocities)
